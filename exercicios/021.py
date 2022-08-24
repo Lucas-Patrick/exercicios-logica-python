4cc615333edcbd752a2b3daeb3de0b8abf06ea7e
Lucas Patrick <111917006+Lucas-Patrick@users.noreply.github.com>
2022-08-24 15:46:14 -0300

    Update 021.py
    """
    Faça um Programa que verifique se uma letra digitada é "F" ou "M".
    Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
    """
    sexo = input("Digite F para Feminino ou M para Masculino: ")
    if sexo == "F":
       sexo = sexo.strip().upper
        print("Feminino")
    elif sexo == "M":
        sexo = sexo.strip().upper
        print("Masculino")
    else:
        print("Sexo Inválido")

--- exercicios/021.py
+++ exercicios/021.py
@@ -3,9 +3,11 @@ Faça um Programa que verifique se uma letra digitada é "F" ou "M".
 Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 """
 sexo = input("Digite F para Feminino ou M para Masculino: ")
-if sexo == "F" or sexo == "f":
+if sexo == "F":
+   sexo = sexo.strip().upper
     print("Feminino")
-elif sexo == "M" or sexo == "m":
+elif sexo == "M":
+    sexo = sexo.strip().upper
     print("Masculino")
 else:
     print("Sexo Inválido")
